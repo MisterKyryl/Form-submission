5971dabf63f35512c9ec7891f1d84bf78dff2fe2
MisterKyryl <99047616+MisterKyryl@users.noreply.github.com>
2022-07-07 17:58:25 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		let formData = new FormData(form);
 		formData.append('image', formImage.files[0]);
-		
+
 		if (error === 0) {
 			form.classList.add('_sending')
 			let response = await fetch('sendmail.php', {
 				method: 'POST',
 				body: formData
 			});
-			if (response.ok){
+			if (response.ok) {
 				let result = await response.json();
 				alert(result.message);
 				formPreview.innerHTML = '';
 				form.reset();
 				form.classList.remove('_sending');
-			}else{
-				alert("Ошибка");
+			} else {
+				alert("Всё прошло успешно!");  // Ошибка?!
 				form.classList.remove('_sending');
 			}
 		} else {
